7f09d3c385bf3dc1a22bb1562953995e56b8d0b9
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-02-23 13:05:05 +0300

    made the webpage more responsive

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function calcAge(){
     var bDate = document.getElementById("bD").value;
     var bMonth = document.getElementById("bM").value;
     var bYear = document.getElementById("bY").value;
+    const label = document.querySelectorAll('.labels');
+    const error = document.querySelectorAll('.error');
 
     var BDval = parseInt(bDate);
     var BMval = parseInt(bMonth);
@@ -28,38 +30,41 @@ function calcAge(){
     }
 
     if(bDate==='' && bMonth==='' && bYear===''){
-        document.getElementById("erD").innerHTML="This field is required";
-        document.getElementById("erD").style.fontStyle="italic";
-        document.getElementById("labelD").style.color="hsl(0, 100%, 67%)";
-        document.getElementById("erM").innerHTML="This field is required";
-        document.getElementById("erM").style.fontStyle="italic";
-        document.getElementById("labelM").style.color="hsl(0, 100%, 67%)";
-        document.getElementById("erY").innerHTML="This field is required";
-        document.getElementById("erY").style.fontStyle="italic";
-        document.getElementById("labelY").style.color="hsl(0, 100%, 67%)";
+        error.forEach((item) => {
+            item.style.visibility="visible";
+            item.innerHTML="This field is required";
+            item.style.fontStyle="italic";
+            item.style.marginBottom="-15px";
+        });
+        
+        label.forEach((item) => {
+            item.style.color="hsl(0, 100%, 67%)";
+            item.style.marginTop="3px";
+        });
     }else if((BDval<0 || BDval>31) && (BMval<0 || BMval>12) && (BYval<0 || BYval>cYear)){
-        document.getElementById("erD").innerHTML="Must be a valid date";
-        document.getElementById("erD").style.fontStyle="italic";
-        document.getElementById("labelD").style.color="hsl(0, 100%, 67%)";
-        document.getElementById("erM").innerHTML="Must a valid month";
-        document.getElementById("erM").style.fontStyle="italic";
-        document.getElementById("labelM").style.color="hsl(0, 100%, 67%)";
-        document.getElementById("erY").innerHTML="Must be in the past";
-        document.getElementById("erY").style.fontStyle="italic";
-        document.getElementById("labelY").style.color="hsl(0, 100%, 67%)";
+        label.forEach((item) => {
+            item.style.color="hsl(0, 100%, 67%)";
+            item.style.marginTop="3px";
+        });
+        error.forEach((item) => {
+            item.style.visibility="visible";
+            item.innerHTML="Enter a valid value";
+            item.style.fontStyle="italic";
+            item.style.marginBottom="-15px";
+        });
     }else{
-        document.getElementById("labelY").style.color="hsl(0, 1%, 44%)";
-        document.getElementById("labelM").style.color="hsl(0, 1%, 44%)";
-        document.getElementById("labelD").style.color="hsl(0, 1%, 44%)";
-        document.getElementById("erD").style.display="none";
-        document.getElementById("erM").style.display="none";
-        document.getElementById("erY").style.display="none";
+        label.forEach((item) => {
+            item.style.color="hsl(0, 1%, 44%)";
+        });
+        error.forEach((item) => {
+            item.style.visibility="hidden";
+        });
 
         if(BYval > cYear || BYval < 0){
             document.getElementById("labelY").style.color="hsl(0, 100%, 67%)";
             document.getElementById("erY").innerHTML="Must be in the past";
             document.getElementById("erY").style.fontStyle="italic";
-            document.getElementById("erY").style.display="block";
+            document.getElementById("erY").style.visibility="visible";
             years = 0;
             months = 0;
             days = 0;
@@ -74,7 +79,7 @@ function calcAge(){
                 document.getElementById("labelD").style.color="hsl(0, 100%, 67%)";
                 document.getElementById("erD").innerHTML="Must be a valid day";
                 document.getElementById("erD").style.fontStyle="italic";
-                document.getElementById("erD").style.display="block";
+                document.getElementById("erD").style.visibility="visible";
             }
         }else if(BMval == 4 || BMval == 6 || BMval == 9 || BMval == 11){
             if(BDval>0 && BDval<=30){
@@ -85,7 +90,7 @@ function calcAge(){
                 document.getElementById("labelD").style.color="hsl(0, 100%, 67%)";
                 document.getElementById("erD").innerHTML="Must be a valid day";
                 document.getElementById("erD").style.fontStyle="italic";
-                document.getElementById("erD").style.display="block";
+                document.getElementById("erD").style.visibility="visible";
             }
         }else if(BMval == 2){
             if(BDval>0 && BDval<=29){
@@ -96,13 +101,13 @@ function calcAge(){
                 document.getElementById("labelD").style.color="hsl(0, 100%, 67%)";
                 document.getElementById("erD").innerHTML="Must be a valid day";
                 document.getElementById("erD").style.fontStyle="italic";
-                document.getElementById("erD").style.display="block";
+                document.getElementById("erD").style.visibility="visible";
             }
         }else{
             document.getElementById("erM").innerHTML="Must be a valid month";
             document.getElementById("erM").style.fontStyle="italic";
             document.getElementById("labelM").style.color="hsl(0, 100%, 67%)";
-            document.getElementById("erM").style.display="block";
+            document.getElementById("erM").style.visibility="visible";
         }
         
     }
